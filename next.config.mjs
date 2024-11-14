6e4ef838c90df6d305810f56e38f7412e18ef6c3
vinney cavallo <vcavallo@gmail.com>
2024-11-14 09:04:51 -0500

    Fix next config re: broken tailwind

--- next.config.mjs
+++ next.config.mjs
@@ -2,24 +2,22 @@
 const nextConfig = {
   output: "export",
   images: {
+    domains: ["general-static-assets.nyc3.digitaloceanspaces.com"],
     unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
   webpack(config) {
-    // Add SVGR loader
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
   },
-
-  // Configure image domains for when you need to reference DO directly
-  images: {
-    domains: ["general-static-assets.nyc3.digitaloceanspaces.com"],
-    unoptimized: true,
+  optimizeFonts: false,
+  experimental: {
+    optimizeCss: false,
   },
 };
 
